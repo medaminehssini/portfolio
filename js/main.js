4e8db31126544e647a5ee185c7ca473d0a051821
medaminehssini <medhssinihssini@gmail.com>
2022-08-03 23:00:07 +0100

    scroll update

--- js/main.js
+++ js/main.js
@@ -88,7 +88,7 @@
         }
     });
     $('.back-to-top').click(function () {
-        $('html, body').animate({ scrollTop: 0 }, 1500, 'easeInOutExpo');
+        $('html, body').animate({ scrollTop: 0 }, 0, 'easeInOutExpo');
         return false;
     });
 
