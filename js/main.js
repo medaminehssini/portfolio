0837711f490ec80754b58bc71be367e986dbc559
medaminehssini <medhssinihssini@gmail.com>
2022-08-22 13:17:23 +0100

    add contact

--- js/main.js
+++ js/main.js
@@ -1,137 +1,93 @@
 (function ($) {
-    "use strict";
+  "use strict";
 
-    // Spinner
-    var spinner = function () {
-        setTimeout(function () {
-            if ($('#spinner').length > 0) {
-                $('#spinner').removeClass('show');
-            }
-        }, 1);
-    };
-    spinner();
+  // Spinner
+  var spinner = function () {
+    setTimeout(function () {
+      if ($("#spinner").length > 0) {
+        $("#spinner").removeClass("show");
+      }
+    }, 1);
+  };
+  spinner();
 
+  // Initiate the wowjs
+  new WOW().init();
 
-    // Initiate the wowjs
-    new WOW().init();
+  // Facts counter
+  $('[data-toggle="counter-up"]').counterUp({
+    delay: 10,
+    time: 2000,
+  });
 
-
-    // Facts counter
-    $('[data-toggle="counter-up"]').counterUp({
-        delay: 10,
-        time: 2000
+  // Typed Initiate
+  if ($(".typed-text-output").length == 1) {
+    var typed_strings = $(".typed-text").text();
+    var typed = new Typed(".typed-text-output", {
+      strings: typed_strings.split(", "),
+      typeSpeed: 100,
+      backSpeed: 20,
+      smartBackspace: false,
+      loop: true,
     });
+  }
 
+  // Smooth scrolling to section
+  $(".btn-scroll").on("click", function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
 
-    // Typed Initiate
-    if ($('.typed-text-output').length == 1) {
-        var typed_strings = $('.typed-text').text();
-        var typed = new Typed('.typed-text-output', {
-            strings: typed_strings.split(', '),
-            typeSpeed: 100,
-            backSpeed: 20,
-            smartBackspace: false,
-            loop: true
-        });
+      $("html, body").animate(
+        {
+          scrollTop: $(this.hash).offset().top - 0,
+        },
+        0,
+        "easeInOutExpo"
+      );
     }
+  });
 
+  // Skills
+  $(".skill").waypoint(
+    function () {
+      $(".progress .progress-bar").each(function () {
+        $(this).css("width", $(this).attr("aria-valuenow") + "%");
+      });
+    },
+    { offset: "80%" }
+  );
 
-    // Smooth scrolling to section
-    $(".btn-scroll").on('click', function (event) {
-        if (this.hash !== "") {
-            event.preventDefault();
+  // Portfolio isotope and filter
+  var portfolioIsotope = $(".portfolio-container").isotope({
+    itemSelector: ".portfolio-item",
+    layoutMode: "fitRows",
+  });
+  $("#portfolio-flters li").on("click", function () {
+    $("#portfolio-flters li").removeClass("active");
+    $(this).addClass("active");
 
-            $('html, body').animate({
-                scrollTop: $(this.hash).offset().top - 0
-            }, 0, 'easeInOutExpo');
-        }
-    });
+    portfolioIsotope.isotope({ filter: $(this).data("filter") });
+  });
 
+  // Testimonials carousel
+  $(".testimonial-carousel").owlCarousel({
+    autoplay: true,
+    smartSpeed: 1500,
+    dots: true,
+    loop: true,
+    items: 1,
+  });
 
-    // Skills
-    $('.skill').waypoint(function () {
-        $('.progress .progress-bar').each(function () {
-            $(this).css("width", $(this).attr("aria-valuenow") + '%');
-        });
-    }, { offset: '80%' });
-
-
-    // Portfolio isotope and filter
-    var portfolioIsotope = $('.portfolio-container').isotope({
-        itemSelector: '.portfolio-item',
-        layoutMode: 'fitRows'
-    });
-    $('#portfolio-flters li').on('click', function () {
-        $("#portfolio-flters li").removeClass('active');
-        $(this).addClass('active');
-
-        portfolioIsotope.isotope({ filter: $(this).data('filter') });
-    });
-
-
-    // Testimonials carousel
-    $(".testimonial-carousel").owlCarousel({
-        autoplay: true,
-        smartSpeed: 1500,
-        dots: true,
-        loop: true,
-        items: 1
-    });
-
-
-    // Back to top button
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 100) {
-            $('.back-to-top').fadeIn('slow');
-        } else {
-            $('.back-to-top').fadeOut('slow');
-        }
-    });
-    $('.back-to-top').click(function () {
-        $('html, body').animate({ scrollTop: 0 }, 0, 'easeInOutExpo');
-        return false;
-    });
-
-
-    //contact 
-    $('#contact').on('submit', function (e) {
-        e.preventDefault();
-        var obj = {
-            table: []
-        };
-
-        var name = $('#name').val();
-        var subject = $('#subject').val();
-        var email = $('#email').val();
-        var message = $('#message').val();
-
-        obj.table.push({ name: name, email: email, subject: subject, message: message });
-
-
-        $.ajax({
-            type: "post",
-            url: "https://us18.list-manage.com/contact-form?u=8f0a8ded6bb04ef4b234e2cb8&form_id=d4bed208e3d43584297f1162a7a60606",
-            data: {
-                fields: {
-                    '930': email,
-                    '934': subject,
-                    '938': message,
-                    '942': name
-                },
-                subscribe: false
-            }
-        }).done(function (response) {
-            console.log(response);
-            $('#name').val('');
-            $('#subject').val('');
-            $('#email').val('');
-            $('#message').val('');
-        });
-
-
-
-
-    })
+  // Back to top button
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $(".back-to-top").fadeIn("slow");
+    } else {
+      $(".back-to-top").fadeOut("slow");
+    }
+  });
+  $(".back-to-top").click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 0, "easeInOutExpo");
+    return false;
+  });
 })(jQuery);
-
-
